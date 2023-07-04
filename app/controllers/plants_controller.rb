83549b01d7cacae6992a5ae60e7ee06d9be67124
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-07-04 17:41:39 +0300

    create action

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,9 +1,20 @@
 class PlantsController < ApplicationController
-    
+
     def index
         plants = Plant.all
         render json: plants
     end
 
+    def show
+        plants = Plant.find(params[:id])
+        render json: plants
+    end
+
+    def create
+        plant = Plant.create(plant_params)
+        render json: plant, status: :created
+    end
 
 end
+
+  
